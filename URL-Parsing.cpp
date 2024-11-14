6ff5dac6a5564bca587e486a53b14191e3d98925
Kewal Nanavati <120007842+kewalll@users.noreply.github.com>
2024-11-14 08:27:08 +0530

    Update URL-Parsing.cpp

--- URL-Parsing.cpp
+++ URL-Parsing.cpp
@@ -3,36 +3,40 @@
 using namespace std;
 
 int main() {
-    string url, protocol = "", host = "", port = "", path = "";
+    string url, protocol, host, port, path;
     cout << "Enter a URL: ";
     cin >> url;
 
-    size_t protocol_pos = url.find("://");
-    if (protocol_pos != string::npos) {
+    // Find protocol
+    int protocol_pos = url.find("://");
+    if (protocol_pos != -1) {
         protocol = url.substr(0, protocol_pos);
         url = url.substr(protocol_pos + 3);
     }
 
-    size_t port_pos = url.find(":");
-    size_t path_pos = url.find("/");
-    if (port_pos != string::npos) {
+    // Find host, port, and path
+    int port_pos = url.find(":");
+    int path_pos = url.find("/");
+    if (port_pos != -1) {
         host = url.substr(0, port_pos);
-        if (path_pos != string::npos) {
+        if (path_pos != -1) {
             port = url.substr(port_pos + 1, path_pos - port_pos - 1);
             path = url.substr(path_pos);
         } else {
             port = url.substr(port_pos + 1);
         }
-    } else if (path_pos != string::npos) {
+    } else if (path_pos != -1) {
         host = url.substr(0, path_pos);
         path = url.substr(path_pos);
     } else {
         host = url;
     }
 
-    cout << "Protocol: " << protocol << endl;
+    // Display results
+    cout << "Protocol: " << (protocol.empty() ? "None" : protocol) << endl;
     cout << "Host/Domain: " << host << endl;
     cout << "Port: " << (port.empty() ? "None" : port) << endl;
     cout << "Path: " << (path.empty() ? "/" : path) << endl;
+
     return 0;
 }
